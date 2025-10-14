7409969cf626ec42cdedb00d9b14805c584f3287
brauliobollainygoytia <105832508+brauliobollainygoytia@users.noreply.github.com>
2025-10-13 23:16:00 -0600

    ya me voy a dormir

--- vamonosRecio_v1.0.sql
+++ vamonosRecio_v1.0.sql
@@ -26,17 +26,17 @@ CREATE TABLE RECORRIDO (
 
 -- INSERCIONES DE LAS RUTAS
 INSERT INTO RUTA VALUES (1, 'Ruta 1','Azul Claro','Lunes-Viernes: 6:20-19:30 Sábados 6:30-19:00 Domingos: 8:00-17:00','8 minutos');
-INSERT INTO RUTA VALUES (2, 'Ruta 1-Paseos del Valle','Azul Claro','Lunes-Viernes: 6:30-19:30 Sábados 6:30-19:30','20 minutos');
-INSERT INTO RUTA VALUES (3, 'Ruta 2','Verde','Lunes-Viernes: 6:00-20:00 Sábados 7:00-19:00 Domingos: 8:00-17:00','15 minutos');
-INSERT INTO RUTA VALUES (4, 'Ruta 3','Gris','Lunes-Viernes: 6:20-20:25','12 minutos');
-INSERT INTO RUTA VALUES (5, 'Ruta 4','Azul Oscuro','Lunes-Viernes: 6:20-20:00 Sábados 7:00-18:00','18 minutos');
-INSERT INTO RUTA VALUES (6, 'Ruta 8','Naranja','Lunes-Viernes: 6:00-20:00 Sábados 6:20-18:00 Domingos: 9:30-16:00','17 minutos');
-INSERT INTO RUTA VALUES (7, 'Ruta 14','Cafe','Lunes-Viernes: 5:30-20:50 Sábados 5:50-20:50 Domingos: 6:20-20:50','14 minutos');
-INSERT INTO RUTA VALUES (8, 'Ruta 15','Morado-Blanco','Lunes-Viernes: 5:25-21:15 Sábados 5:25-20:50 Domingos: 6:00-20:50','10 minutos');
-INSERT INTO RUTA VALUES (9, 'Ruta 16','Morado-Amarillo','Lunes-Viernes: 5:45-10:05 Sábados 6:00-22:05 Domingos: 6:00-22:05','9 minutos');
-INSERT INTO RUTA VALUES (10, 'Ruta 17','Verde-Amarillo','Lunes-Viernes: 5:30-20:40 Sábados 5:40-20:40 Domingos: 6:00-20:40','8 minutos');
-INSERT INTO RUTA VALUES (11, 'Transportes de Guadalupe','Rojo-Blanco','Lunes-Viernes: 5:30-21:15 Sábados-Domingos 6:00-21:15','12 minutos');
-INSERT INTO RUTA VALUES (12, 'Tierra y Libertad','Rojo-Blanco','Lunes-Viernes: 5:30-21:21 Sábados 6:00-21:15 Domingos: 6:00-21:15','11 minutos');
+INSERT INTO RUTA VALUES (20, 'Ruta 1-Paseos del Valle','Azul Claro','Lunes-Viernes: 6:30-19:30 Sábados 6:30-19:30','20 minutos');
+INSERT INTO RUTA VALUES (2, 'Ruta 2','Verde','Lunes-Viernes: 6:00-20:00 Sábados 7:00-19:00 Domingos: 8:00-17:00','15 minutos');
+INSERT INTO RUTA VALUES (3, 'Ruta 3','Gris','Lunes-Viernes: 6:20-20:25','12 minutos');
+INSERT INTO RUTA VALUES (4, 'Ruta 4','Azul Oscuro','Lunes-Viernes: 6:20-20:00 Sábados 7:00-18:00','18 minutos');
+INSERT INTO RUTA VALUES (8, 'Ruta 8','Naranja','Lunes-Viernes: 6:00-20:00 Sábados 6:20-18:00 Domingos: 9:30-16:00','17 minutos');
+INSERT INTO RUTA VALUES (14, 'Ruta 14','Cafe','Lunes-Viernes: 5:30-20:50 Sábados 5:50-20:50 Domingos: 6:20-20:50','14 minutos');
+INSERT INTO RUTA VALUES (15, 'Ruta 15','Morado-Blanco','Lunes-Viernes: 5:25-21:15 Sábados 5:25-20:50 Domingos: 6:00-20:50','10 minutos');
+INSERT INTO RUTA VALUES (16, 'Ruta 16','Morado-Amarillo','Lunes-Viernes: 5:45-10:05 Sábados 6:00-22:05 Domingos: 6:00-22:05','9 minutos');
+INSERT INTO RUTA VALUES (17, 'Ruta 17','Verde-Amarillo','Lunes-Viernes: 5:30-20:40 Sábados 5:40-20:40 Domingos: 6:00-20:40','8 minutos');
+INSERT INTO RUTA VALUES (21, 'Transportes de Guadalupe','Rojo-Blanco','Lunes-Viernes: 5:30-21:15 Sábados-Domingos 6:00-21:15','12 minutos');
+INSERT INTO RUTA VALUES (22, 'Tierra y Libertad','Rojo-Blanco','Lunes-Viernes: 5:30-21:21 Sábados 6:00-21:15 Domingos: 6:00-21:15','11 minutos');
 
 -- INSERCIONES DE LAS PARADAS
 INSERT INTO PARADAS VALUES (1, '2 de Marzo', 22.774726, -102.588541);
@@ -446,3 +446,22 @@ INSERT INTO PARADAS VALUES (405, 'Walmart Colinas', 22.749153, -102.591148);
 INSERT INTO PARADAS VALUES (406, 'Walmart Colinas II', 22.749198, -102.591384);
 INSERT INTO PARADAS VALUES (407, 'Zacatecas 1', 22.753999, -102.496746);
 INSERT INTO PARADAS VALUES (408, 'Walmart Colinas', 22.749153, -102.591148);
+
+
+-- Inserciones ruta 4
+
+INSERT INTO RECORRIDO VALUES(4,131);
+INSERT INTO RECORRIDO VALUES(4,128);
+INSERT INTO RECORRIDO VALUES(4,147);
+INSERT INTO RECORRIDO VALUES(4,309);
+INSERT INTO RECORRIDO VALUES(4,78);
+INSERT INTO RECORRIDO VALUES(4,179);
+INSERT INTO RECORRIDO VALUES(4,247);
+INSERT INTO RECORRIDO VALUES(4,189);
+INSERT INTO RECORRIDO VALUES(4,173);
+INSERT INTO RECORRIDO VALUES(4,68);
+INSERT INTO RECORRIDO VALUES(4,320);
+INSERT INTO RECORRIDO VALUES(4,120);
+INSERT INTO RECORRIDO VALUES(4,241);
+INSERT INTO RECORRIDO VALUES(4,128);
+
